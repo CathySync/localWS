ee8c030b6a05b2b287088f855db742c62922a982
sproj <deathtohotmail1@gmail.com>
2015-08-31 02:23:23 +0100

    nested project, task, translation broken
    fucked.

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET companies array. */
+/* GET projects array. */
 router.get('/', function(req, res) {
     var conn = req.conn;
-    conn.query('SELECT * FROM `projects`', function(err, results, fields){
+    var projects = [];
+    var projquery = conn.query('SELECT * FROM `project`', function(err, results, fields){
         if(err) throw err;
-        res.json(results);
-    });
+        projects = results;
+        res.json({"projects": projects});
+        });
 });
+/*
+function getProjectTasks(proj){
+    var projId = proj.proj_projectid;
+    var tasks = [];
+    //Get the tasks.
+    var taskquery = conn.query('SELECT * from `task` WHERE task_projectid = '+ projId, function(err, results, fields){
+                    if(err) throw err;
+                    tasks = results;
+                    });
+    taskquery.on('end', function(){
+        console.log(tasks);
+    })
+    //tasks = tasks.map(getTaskTranslations(task));
+//return tasks;
+}
 
+function getTaskTranslations(task){
+    var taskId = task.task_taskid;
+    //Get the translations to each task
+    var translations = conn.query('SELECT * from `translation` WHERE tran_taskid = '+ taskId, function(err, results, fields){
+            if(err) throw err;
+    return results;
+    });
+    console.log(translations); 
+    task["translations"] = translations;
+return task;
+}
+*/
 module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET companies array. */
+/* GET tasks array. */
 router.get('/', function(req, res) {
     var conn = req.conn;
-    conn.query('SELECT * FROM `tasks`', function(err, results, fields){
+    var tasks = [];
+    var projquery = conn.query('SELECT * FROM `task`', function(err, results, fields){
         if(err) throw err;
-        res.json(results);
-    });
+        tasks = results;
+        res.json({"tasks": tasks});
+        });
 });
 
 module.exports = router;
