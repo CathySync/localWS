be4f4181a6c0296362bee5e4a7e7ac6f80c1ef95
sproj <deathtohotmail1@gmail.com>
2015-08-31 09:45:31 +0100

    nested projects, tasks, translations outputting

--- routes/projects.js
+++ routes/projects.js
@@ -5,11 +5,50 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var conn = req.conn;
     var projects = [];
+    var tasks = [];
+    var translations = [];
+
     var projquery = conn.query('SELECT * FROM `project`', function(err, results, fields){
         if(err) throw err;
         projects = results;
-        res.json({"projects": projects});
         });
+        //When the project query returns, nest each project with its tasks, and each tasks translations to output
+        projquery.on('end', function(){
+
+        var taskquery = conn.query('SELECT * from `task`', function(err, results, fields){
+                if(err) throw err;
+                tasks = results;
+            });
+
+        taskquery.on('end', function(){
+            var translationsquery = conn.query('SELECT * from `translation`', function(err, results, fields){
+                if(err) throw err;
+                translations = results;
+                //Give each task its translations, and each project its tasks.
+                var nestedTaskTrans = tasks.map(function(el){
+                    var taskid = el.task_taskid
+                    var scopeTrans = translations.filter(function(tran){
+                        if(tran.tran_taskid == taskid) return tran;
+                    });
+                    el["translations"] = scopeTrans;
+                    return el;
+                });
+
+            var nestedprojectTasks = projects.map(function(proj){
+                var projid = proj.proj_projectid;
+                            //Give each project its tasks
+                var nestedProjectTasks = nestedTaskTrans.filter(function(ntt){
+                    if(ntt.task_taskid == projid) return ntt;
+                            });
+                    proj["tasks"] = nestedProjectTasks;
+                            return proj;
+                        });
+                        //console.log(nestedprojectTasks);
+                        res.json({"projects": nestedprojectTasks});
+                    });
+                });
+    });
+
 });
 /*
 function getProjectTasks(proj){
